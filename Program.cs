82826296b8a68bdc213a35345d22b71ad0686cd8
Kauadtg <kauadtg@gmail.com>
2025-08-19 00:32:35 -0300

    Atualizando com meu código atual

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Testando o Nokia:");
+        Nokia nokia = new Nokia("22998867525", "Nokia Tijolão", "123456789012345", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+        
+        Console.WriteLine("\n");
+        
+        Console.WriteLine("\n");
+        Console.WriteLine("Testando o iPhone:");
+        Iphone iphone = new Iphone("22997725541", "iPhone 16 Pro Max", "987654321098765", 256);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
